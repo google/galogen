e258b1c8ac0e8c9e89d0e9271ed79d8351539827
nicebyte <nicebyte@mailinator.com>
2018-05-07 16:31:58 -0700

    Load wglGetProcAddress dynamically
    Fixes crash under RenderDoc

--- galogen.cpp
+++ galogen.cpp
@@ -902,14 +902,22 @@ extern "C" {
 const char *get_proc_address_code = R"STR(
 
 #if defined(_WIN32)
-
 void* GalogenGetProcAddress(const char *name) {
   static HMODULE opengl32module = NULL;
-  void *ptr = (void *)wglGetProcAddress(name);
+  static PROC(WINAPI *wgl_get_proc_address)(LPCSTR name) = NULL;
+  if (!wgl_get_proc_address) {
+    if (!opengl32module) {
+      opengl32module = LoadLibraryA("opengl32.dll");
+    }
+    wgl_get_proc_address = (PROC)GetProcAddress(opengl32module, "wglGetProcAddress");
+    assert(wgl_get_proc_address);
+  }
+  void *ptr = (void *)wgl_get_proc_address(name);
   if(ptr == 0 || (ptr == (void*)1) || (ptr == (void*)2) || (ptr == (void*)3) ||
      (ptr == (void*)-1) ) {
     if (opengl32module == NULL) {
       opengl32module = LoadLibraryA("opengl32.dll");
+      assert(opengl32module);
     }
     ptr = (void *)GetProcAddress(opengl32module, name);
   }
